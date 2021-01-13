2bf97712214cec0ce7b531215cb3f9d2cc9d26a6
Andcrit <tiknathearsim@gmail.com>
2021-01-13 18:34:31 -0500

    Updated the package.json to include the Express dependacies. Also added the server file for the backend setup

---
+++ my-app/server.js
@@ -0,0 +1,11 @@
+//This is the server 
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(4200, function () {
+  console.log('Example app listening on port 4200!');
+});
