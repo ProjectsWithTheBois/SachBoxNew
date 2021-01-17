2c9f10fb4e8b63ca14d9acaa4b326b46217f7664
SachJay <42943459+SachJay@users.noreply.github.com>
2021-01-17 01:12:06 -0500

    Revert "updates base App component to setup routing with stubbed endpoints (#7)" (#8)
    This reverts commit efe5a3455e1acdf081d37e82dab7f32633f79075.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,34 +1,24 @@
+import logo from './logo.svg';
 import './App.css';
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Switch> {/*you can change the endpoint name of your ticket to be more accurate to the page*/}
-          <Route exact path="/">
-            <p>Replace this with home page component</p>
-          </Route>
-          <Route path="/about">
-            <p>Replace this with about page component</p>
-          </Route>
-          <Route path="/gameselect">
-            <p>Replace this with game select component</p>
-          </Route>
-          <Route path="/joingame">
-            <p>Replace this with join game component</p>
-          </Route>
-          <Route exact path="/beachspeech"> {/*may have to create sub routes depending on how we create the game*/}
-            <p>Replace this with game lobby component</p>
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
