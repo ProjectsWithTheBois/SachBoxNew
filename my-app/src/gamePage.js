646cae5bacb6986eb26e5472a6fdbdfd83859ea9
Andcrit <tiknathearsim@gmail.com>
2021-01-17 15:01:21 -0500

    Work in progress for adding the gamepage

---
+++ my-app/src/gamePage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function GamePage(){
+
+return (
+<div>
+    <header> 
+        <title> Project with the BOIS </title>
+    </header>
+    <img src = ""> </img>
+    <img src = ""> </img>
+    <img src = ""> </img>
+    <img src = "">  </img>
+    <body>
+        <p> This is our game page</p>
+    </body>
+    <footer>
+    </footer>
+</div>
+);
+}
+export default GamePage
