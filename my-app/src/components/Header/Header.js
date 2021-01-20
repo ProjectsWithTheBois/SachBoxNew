dae57c99e8b5b17e3a8af2914b8ed9c46b666bf4
Luke Bacchus <luke.bacchus@mail.utoronto.ca>
2021-01-20 16:44:45 -0500

    Made a header

---
+++ my-app/src/components/Header/Header.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { uid } from 'react-uid';
+import { Button } from '@material-ui/core';
+import './Header.css';
+
+const NAV_LINKS = [
+    {
+        label: 'About Us',
+        path: '/about'
+    }, 
+    {
+        label: 'Game Select',
+        path: '/gameselect'
+    },
+    {
+        label: 'Join Game',
+        path: '/joingame'
+    },
+    {
+        label: 'Beach Speech',
+        path: '/beachspeeech'
+    }
+]
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div id="header">
+                <div id="jumplist">
+                    {NAV_LINKS.map((navItem) => (
+                <Link 
+                    key={uid(navItem)}
+                    to={{
+                        pathname: navItem.path,
+                    }}> 
+                    <Button color="primary">{navItem.label}</Button>
+                </Link>
+                    ))}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default withRouter(Header);
