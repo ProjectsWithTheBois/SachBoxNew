7453d00ba911b6561969fa2deb039456db237f10
Renascitur <hanzala010@gmail.com>
2021-01-21 18:00:43 -0500

    send a image

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 //This is the server 
+var path = require('path');
 var express = require('express');
 var app = express();
 
+let resources = path.join(__dirname, 'resources');
+
+app.get('/image', function (req, res) {
+  res.sendFile(resources + '/AlwaysWatching.png');
+});
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Bye World!');
 });
 
 app.listen(4200, function () {
