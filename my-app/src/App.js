cc0b33a116f62e3df416843bf71d6530bd1215ea
LukeBacchus <47614906+LukeBacchus@users.noreply.github.com>
2021-01-21 15:40:11 -0500

    Header (#14)
    * Made a header
    
    * updated dependencies
    
    * Small formatting fixes
    
    Co-authored-by: Luke Bacchus <luke.bacchus@mail.utoronto.ca>

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,28 +7,29 @@ import {
 } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Header from '../src/components/Header/Header'
+
 function App() {
   return (
     <Router>
-      <div>
-        <Switch> {/*you can change the endpoint name of your ticket to be more accurate to the page*/}
-          <Route exact path="/">
-            <p class="text-danger">Replace this with home page component</p> {/*Example of bootstrap, need import (line 8)*/}
-          </Route>
-          <Route path="/about">
-            <p>Replace this with about page component</p>
-          </Route>
-          <Route path="/gameselect">
-            <p>Replace this with game select component</p>
-          </Route>
-          <Route path="/joingame">
-            <p>Replace this with join game component</p>
-          </Route>
-          <Route exact path="/beachspeech"> {/*may have to create sub routes depending on how we create the game*/}
-            <p>Replace this with game lobby component</p>
-          </Route>
-        </Switch>
-      </div>
+      <Header></Header>
+      <Switch> {/*you can change the endpoint name of your ticket to be more accurate to the page*/}
+        <Route exact path="/">
+          <p class="text-danger">Replace this with home page component</p> {/*Example of bootstrap, need import (line 8)*/}
+        </Route>
+        <Route path="/about">
+          <p>Replace this with about page component</p>
+        </Route>
+        <Route path="/gameselect">
+          <p>Replace this with game select component</p>
+        </Route>
+        <Route path="/joingame">
+          <p>Replace this with join game component</p>
+        </Route>
+        <Route exact path="/beachspeech"> {/*may have to create sub routes depending on how we create the game*/}
+          <p>Replace this with game lobby component</p>
+        </Route>
+      </Switch>
     </Router>
   );
 }
