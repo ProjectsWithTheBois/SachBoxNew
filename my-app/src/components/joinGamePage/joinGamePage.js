6afeae88f2fdeda46d8632f53d3271687e34a069
IqbalBan <banwait.iqbal@gmail.com>
2021-01-23 13:07:40 -0500

    Added comments and fixed the spacing.

--- my-app/src/components/joinGamePage/joinGamePage.js
+++ my-app/src/components/joinGamePage/joinGamePage.js
@@ -1,9 +1,8 @@
 import React from 'react';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 function JoinGamePage(){
     return (
-        
         <div>
             <title> Project with the BOIS </title>
             <div className ="container">
@@ -11,6 +10,7 @@ function JoinGamePage(){
                     <div className ="form-group">
                         <label for="text">ROOM CODE</label>
                         <input type="text" class="form-control" id="inputRoomCode" placeholder="ENTER 4-LETTER CODE"  maxlength="4"  autocapitalize="off" autocorrect="off" autocomplete="off"/>
+                        {/*type/class is basic bootstrap, placeholder is what appears in the bar when it isn't filled in, maxlength sets max length of input (4 digit code), autocaptializing, autocorrect and autocomplete when you input text is all turned off */}
                     </div>
                     <div class="form-group">
                         <label for="exampleInputPassword1">NAME</label>
